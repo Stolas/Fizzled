612c7bfe49943b266a04d714317e942d7ecff0d3
Robin Vossen <robin@codeinject.org>
2015-09-29 13:29:28 +0200

    Included the big about fuzzing statergies

--- mutilator.py
+++ mutilator.py
@@ -14,6 +14,16 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
+def charlie_miller_fuzz(buf):
+    # Charlie Miller's fuzzer code:
+    num_writes = randrange(math.ceil((float(len(buf)) / fuzz_factor))) + 1
+
+    for _ in range(num_writes):
+        r_byte = randrange(256)
+        rn = randrange(len(buf))
+        buf[rn] = r_byte
+    # end of Charlie Miller's code
+    return buf
 
 def run(seed_dir, samples_dir):
     file_list = [f for f in listdir(seed_dir) if isfile(join(seed_dir, f))]
@@ -27,14 +37,8 @@ def run(seed_dir, samples_dir):
             buf = bytearray(fd.read())
             fd.close()
 
-            # Charlie Miller's fuzzer code:
-            num_writes = randrange(math.ceil((float(len(buf)) / fuzz_factor))) + 1
-
-            for _ in range(num_writes):
-                r_byte = randrange(256)
-                rn = randrange(len(buf))
-                buf[rn] = r_byte
-            # end of Charlie Miller's code
+            # XXX: Hack a good fuzzy strategy here
+            buf = charlie_miller_fuzz(buf)
 
             new_filename = join(samples_dir, time())
             logger.debug('Creating file: {}'.format(new_filename))
